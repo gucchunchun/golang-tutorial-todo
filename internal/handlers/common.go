30097a3191120285de4a837f39101ed56bfd9300
gucchunchun <yunataniguchi23@gmail.com>
2025-08-15 10:28:29 +0900

    refactor: handlerにリスポンスヘルパー関数移動

---
+++ internal/handlers/common.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// NOTE: ヘルパー関数のためanyを許容
+func writeJSON(w http.ResponseWriter, statusCode int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func writeJSONError(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
--- internal/httpapi/server.go
+++ internal/httpapi/server.go
@@ -3,17 +3,9 @@ package httpapi
 import (
 	"encoding/json"
 	"net/http"
-
-	"golang/tutorial/todo/internal/models"
 )
 
-type TaskService interface {
-	// Create(title string) error
-	// Get(id string) (models.Task, error)
-	List() ([]models.Task, error)
-	// Update(id string, title string) error
-	// Delete(id string) error
-}
+// type TaskHandler intercace
 
 type Server struct {
 	// TaskService TaskService
@@ -31,19 +23,8 @@ func (s *Server) Routes() http.Handler {
 	return mux
 }
 
-// NOTE: ヘルパー関数のためanyを許容
-func writeJSON(w http.ResponseWriter, statusCode int, v any) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	_ = json.NewEncoder(w).Encode(v)
-}
-
-func writeJSONError(w http.ResponseWriter, statusCode int, message string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	_ = json.NewEncoder(w).Encode(map[string]string{"error": message})
-}
-
 func (s *Server) handleHelloworld(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]string{"message": "Hello, World!"})
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
 }
