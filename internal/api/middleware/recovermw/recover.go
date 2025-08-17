ca285cc0e1f4b1f61a5ed1028dcca90aad745ac5
gucchunchun <yunataniguchi23@gmail.com>
2025-08-17 21:51:13 +0900

    add: ログを追加

--- internal/api/middleware/recovermw/recover.go
+++ internal/api/middleware/recovermw/recover.go
@@ -2,14 +2,21 @@ package recovermw
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 )
 
+/**
+* Reference: O'REILLY「実用GO言語」10.5.3 p.246
+* panic発生時でもサーバープロセスが終了せず、別のリクエストを受け続けられるようにする
+ */
 func Recover(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
+				// TODO: DBロールバックの実装
+				log.Println(err)
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusInternalServerError)
 				json.NewEncoder(w).Encode(map[string]any{
