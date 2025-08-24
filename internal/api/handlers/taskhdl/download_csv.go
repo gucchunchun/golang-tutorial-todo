eab9868437975c23c3ebf1fba4546e9ac3afe55a
gucchunchun <yunataniguchi23@gmail.com>
2025-08-24 17:43:55 +0900

    change: デバッグ用コメントを削除

--- internal/api/handlers/taskhdl/download_csv.go
+++ internal/api/handlers/taskhdl/download_csv.go
@@ -8,39 +8,32 @@ import (
 )
 
 func (t *TaskHandler) downloadCSV(w http.ResponseWriter, r *http.Request) {
-	t.Logger.Debug().Msg("---downloadCSV called---")
+	t.Logger.Debug().Msg("--- downloadCSV ---")
 	// CSVダウンロード処理
 	w.Header().Set("Content-Type", "text/csv")
 	w.Header().Set("Content-Disposition", "attachment; filename=tasks.csv")
 
 	// Taskの読み込み
-	t.Logger.Debug().Msg("fetching task data")
 	tasks, err := t.TaskService.ListTasks()
 	if err != nil {
 		t.Logger.Error().Err(err).Msg("failed to fetch tasks")
 		handlers.WriteError(w, err)
 		return
 	}
-	t.Logger.Debug().Msg("fetching task data: ok")
 
 	// CSVの書き込み
-	t.Logger.Debug().Msg("csv.Writer creation")
 	cw := csv.NewWriter(w)
-	t.Logger.Debug().Msg("csv.Writer creation: ok")
 	// 	バッファにあるデータを書き込み
 	defer cw.Flush()
 
 	// ヘッダーの書き込み
-	t.Logger.Debug().Msg("write header")
 	if err := cw.Write([]string{"ID", "Name", "Status", "Due Date", "TimeLeft"}); err != nil {
 		t.Logger.Error().Err(err).Msg("failed to write CSV header")
 		handlers.WriteError(w, err)
 		return
 	}
-	t.Logger.Debug().Msg("write header: ok")
 
 	// 各タスクの書き込み
-	t.Logger.Debug().Msg("write tasks")
 	for _, task := range tasks {
 		// TODO: nilチェックが必要ないようにTask, TaskOutputを修正する
 		dueDateAt := ""
@@ -63,7 +56,6 @@ func (t *TaskHandler) downloadCSV(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	t.Logger.Debug().Msg("write tasks: ok")
 
 	if cw.Error() != nil {
 		t.Logger.Error().Err(cw.Error()).Msg("failed to write CSV")
